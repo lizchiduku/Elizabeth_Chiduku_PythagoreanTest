fad94337f66bf316f01d054558318f6531179b70
lizchiduku <50470205+lizchiduku@users.noreply.github.com>
2019-06-16 20:47:24 -0500

    Add files via upload

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest{
+	public static void main(String[] args){
+		Pythagorean result = New Pythagorean();
+		double output = result.calculateHypotenuse(4,6);
+	System.out.println(output);
+	}
+}
